21d53dbf46aa3efeffc87e91f0ac0a22e0ba51ba
Oleksii Honchar <tuiteraz@gmail.com>
2020-09-21 21:29:06 +0300

    docs(updated about):

--- src/pages/Root/components/About.tsx
+++ src/pages/Root/components/About.tsx
@@ -13,25 +13,29 @@ export function About(): ReactElement {
         About
       </h2>
       <p>
-        Hello! My name is Alex. I’m an GettingThingDone IT guy with a long story
-        about software development and team management in different domains, processes
-        and technologies. Long time ago I started as an intern within one successful
-        gaming startup. Year after year I was constantly learning, coding and accepting
-        the challenges. Until once I’ve discovered myself helping others to conquer their
-        peaks.  Already 5+ years I’m practicing team management, leading and coaching for
-        teams from 2 up to 40 people.
+        Hello! My name is Alex. I&apos;m a GettingThingDone IT guy with a long story about
+        software development and team management in different domains, processes and
+        technologies. Long time ago I started as an intern within one successful gaming
+        startup. Year after year I was constantly learning, coding and accepting the
+        challenges. Until once I&apos;ve discovered myself helping others to conquer their peaks.
+        I&apos;m leading and coaching teams from 2 up to 40 people for more than 5 years.
+        And I love it!
       </p>
       <p>
-        Driven by business needs I choose the best solution and architecture for quick
-        and long wins. Constructive and consistent approach helps me to conquer uncertainty.
-        Ready to take responsibility, execute and delegate for best performance.
+        My experience covers such areas as: finance, e-commerce, healthcare, advertising.
+        The scale of projects was from start-up to marketplace. My role also was quite
+        diverse. Usually it was a mix of team managing, process setup, requirement
+        clarification and actual development.
       </p>
       <p>
-        I’m excited to drive and join development process using agile
-        practices for task management and DevOps. Among all personal qualities I
-        would like to notice high personal standards and attention to details.
+        I would describe myself as a highly autonomous and collaborative problem solver.
+        I&apos;m strong in putting things together and making it work. I&apos;m good at seeing and
+        revealing people&apos;s potential. Appreciate responsibility, diligence, good teamwork
+        and desire to grow.
       </p>
-      <p>I&apos;m glad to cooperate!</p>
+      <p>I&apos;m looking for a promising business idea or product where I can apply my entire
+        skill set to achieve the best possible result.
+      </p>
     </section>
   );
 }
